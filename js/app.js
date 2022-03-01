2f404da81427feda4d5403690bf9ed0dfed34fa8
sakilshikdar <sakilshikdar2504@gmail.com>
2022-02-28 21:45:42 -1200

    phone-showed

--- js/app.js
+++ js/app.js
@@ -2,6 +2,32 @@ const searchPhone = () => {
     const searchField = document.getElementById('search-field')
     const searchText = searchField.value;
     searchField.value ='';
-    console.log(searchText)
+    // console.log(searchText)
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data =>displayPhone(data.data))
 }
+
+const displayPhone = (brand) => {
+    // console.log(brand)
+     
+    const searchResult = document.getElementById('search-result')
+    for(const brands of brand){
+        // console.log(brands.image)
+        const div = document.createElement('div')
+        div.classList.add('col')
+        div.innerHTML = `
+         <img src="${brands.image}" class="w-75 card-img-top" alt="...">
+         <div class="card-body">
+           <h6>Name: ${brands.phone_name}</h6>
+            <h5>Brand: ${brands.brand}</h5>
+         </div>
+        </div>
+        `
+        searchResult.appendChild(div)
+    }
+
+}
+
     
