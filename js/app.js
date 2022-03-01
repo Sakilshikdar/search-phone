22d4282e317a5f8721bbb90dce4cba16c093a38f
sakilshikdar <sakilshikdar2504@gmail.com>
2022-02-28 20:44:51 -1200

    first commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const searchPhone = () => {
+    const searchField = document.getElementById('search-field')
+    const searchText = searchField.value;
+    searchField.value ='';
+    console.log(searchText)
+}
+    
